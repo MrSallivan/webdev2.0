258ef59cecaf297527611b9319e97f029a02cf63
MrSallivan <nik.sallivan@gmail.com>
2020-11-18 23:32:42 +1000

    jq_codeplayer

--- 4-jquery/my/jqUI/script.js
+++ 4-jquery/my/jqUI/script.js
@@ -1,14 +1,26 @@
-$('.drag').draggable({cursor: 'move', cursorAt: {top:0, left:0}, axis:'y'})
-$('p').draggable({containment: "parent"})
+$('.drag').draggable({ cursor: 'move', cursorAt: { top: 0, left: 0 }, axis: 'y' })
+$('p').draggable({ containment: "parent" })
 
 $('.resizeble').resizable({
 	grid: 50,
 	alsoResize: '.also',
-	resize: function(event, ui) {
-		if ( $('.resizeble').width() > 300 ) {
+	resize: function (event, ui) {
+		if ($('.resizeble').width() > 300) {
 			alert('Стой разогнался!!')
 		}
 	}
 })
 
 $('.also').resizable()
+$('.small').draggable({ cursor: 'move' })
+$('.big').droppable({
+	drop: function (event, ui) {
+		console.log(event)
+		console.log(ui)
+		$(this).addClass('blue').find('.par').html('Круто!')
+	}
+})
+
+$("#accordion").accordion({
+	collapsible: true
+});
