ff4300f8ad82c09cc0343e10d2b29912f01f397b
MrSallivan <nik.sallivan@gmail.com>
2020-11-20 17:43:08 +1000

    playcode_done

--- 4-jquery/my/codePlayer/script.js
+++ 4-jquery/my/codePlayer/script.js
@@ -1,3 +1,8 @@
+function updateOutput() {
+    $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+    document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+}
+
 $('.toggleButton').hover(function () {
 	$(this).addClass('highlighted')
 }, function () {
@@ -8,13 +13,17 @@ $('.toggleButton').click(function () {
 	$(this).removeClass('highlighted')
 
 	let panelId = $(this).attr('id') + 'Panel'
-	$('#' + panelId).toggle()
+	$('#' + panelId).toggleClass('hidden')
+	let numberOfActivePanel = 4 - $('.hidden').length
+	$('.panel').width(($(window).width() / numberOfActivePanel) - 10)
 })
 
 $('textarea').height($(window).height() - $('#header').height() - 20)
 $('.panel').height($(window).height() - $('#header').height() - 20)
 $('.panel').width(($(window).width() / 2) - 10)
 
+updateOutput()
+
 $('textarea').on('change keyup paste', function () {
-	$('iframe').contents().find('html').html($('#htmlPanel').val())
+	updateOutput()
 })
