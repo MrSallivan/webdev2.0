adce1a616a0dddb03d927282c2fd7f02430b7da2
MrSallivan <nik.sallivan@gmail.com>
2020-11-22 07:40:05 +1000

    mybootapp

--- 5-bootstrap/my/script.js
+++ 5-bootstrap/my/script.js
@@ -1,1 +1,12 @@
-script
+$('#mymodal').on('show.bs.modal', function(event){
+	let button = $(event.relatedTarget)
+	let recipient = button.data('name')
+	let modal = $(this)
+	modal.find('.modal-title').text(`Сообщение для ${recipient} кнопки`)
+})
+$('#mymodal').on('hidden.bs.modal', function(event){
+	$('#alertmodal').removeClass('d-none').css({
+		'position': 'absolute', 
+		'top': '350px', 
+		'right':'100px'})
+})
